9071fc3c767f22576adda965330ef0f44412fbeb
Tobias Abarbanell <tobias.abarbanell@frontiersin.org>
2015-04-15 17:24:31 +0200

    extended run script

--- run.sh
+++ run.sh
@@ -3,6 +3,11 @@
 boot2docker up
 $(boot2docker shellinit)
 
+
+echo remove mongo-dev container if exists
+docker rm -f mongo-dev
+
+echo start new mongo-dev container
 docker run --name mongo-dev -d -p 27017:27017 mongo
 
 echo docker is running on host `boot2docker ip`
